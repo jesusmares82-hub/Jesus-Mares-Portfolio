86ae220961a221e63ab236a601b0bc9f41388d12
Jesus <jesusmares82@gmail.com>
2020-12-28 13:08:57 -0600

    first commit

---
+++ JS/loader.js
@@ -0,0 +1,7 @@
+//======================================================================
+// LOADING
+//======================================================================
+window.addEventListener("load", function () {
+  console.log(document.getElementById("loader"));
+  document.getElementById("loader").classList.toggle("loader2");
+});
