0bea48fb0a9dd806c5325a72a3a1bbad91f8f856
Jesus <jesusmares82@gmail.com>
2021-01-01 12:45:39 -0600

    v.0.1

--- JS/loader.js
+++
@@ -1,7 +0,0 @@
-//======================================================================
-// LOADING
-//======================================================================
-window.addEventListener("load", function () {
-  console.log(document.getElementById("loader"));
-  document.getElementById("loader").classList.toggle("loader2");
-});
